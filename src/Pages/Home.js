f96639a45d4e9b6e15ae6fa1f29969ad19807808
shvrekhan <shvrekhan@gmail.com>
2023-01-31 10:52:41 +0530

    Homepage implementation started and signup form added with validation

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Components/Header/Header";
+import SignUp from "../Components/SignUp/SignUp";
+
+class Home extends React.Component {
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
+
+export default Home;
