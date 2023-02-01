825fb0c5327903e2c43f33407ec4c7adf2374984
shvrekhan <shvrekhan@gmail.com>
2023-02-01 14:53:20 +0530

    search bar added in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css"
-import SignUp from "../SignUp/SignUp";
+import Navbar from "../Navbar/Navbar";
+import { Link } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 class Header extends React.Component {
     constructor(props) {
@@ -19,10 +21,18 @@ class Header extends React.Component {
     render() {
         return (
             <>
-                {/* <button onClick={this.handelSignup}>Signup</button>
-                {this.state.isSignup === true && <SignUp />} */}
+                <div className="head-container container-fluid d-flex flex-column">
+                    <Navbar />
+                    <div className="zomato-image-section d-flex align-self-center">
+                        <img className="zomato-center-image "
+                            src="https://b.zmtcdn.com/web_assets/8313a97515fcb0447d2d77c276532a511583262271.png" />
+                    </div>
+                    <div className="head-info-section">
+                        <h1>Discover the best food & drinks in Bengaluru</h1>
+                    </div>
+                    <SearchBar />
 
-                <SignUp />
+                </div>
             </>
         )
     }
