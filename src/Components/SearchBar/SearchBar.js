b10440f8cf3973f629ab55c631a9db5b83e42cce
shvrekhan <shvrekhan@gmail.com>
2023-02-01 14:54:16 +0530

    search bar implemented

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import "./SearchBar.css"
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <>
+                <div className='search-bar-container d-flex align-self-center pb-4'>
+                    <div className='location-input-container d-flex'>
+                        <div className='fa-sharp fa-solid fa-location-dot fa-2x location-icon-custom-css'></div>
+                        <input placeholder='Bengaluru' className='location-input' />
+                        <div className='fa-solid fa-caret-up  triangle-icon-custom-css'></div>
+                    </div>
+                    <div className='search-input-container d-flex'>
+                        <div className='fa-solid fa-magnifying-glass  search-icon-custom-css'></div>
+                        <input placeholder='Search for restaurant, cuisine or a dish' className='search-input' />
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default SearchBar;
