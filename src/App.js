184d2dbbe680b27ff6aa0be00e9f2e472232f95a
shvrekhan <shvrekhan@gmail.com>
2023-02-01 14:56:11 +0530

    redux integrated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./Pages/Home/Home";
 
 class App extends React.Component {
   render() {
