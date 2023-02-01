f0992fb7ef76590a7a57afa3c9f1a6c1a81e7840
shvrekhan <shvrekhan@gmail.com>
2023-02-01 14:56:55 +0530

    bootstrap module removed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import Store from "./redux/store"
