1c520a6dad35001e0cbaa83a132ba33b39572a31
shvrekhan <shvrekhan@gmail.com>
2023-02-01 16:27:23 +0530

    collection part added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CardHotelCategory from "../../Components/CardHotelCategory/CardHotelCategory";
+import Collections from "../../Components/Collections/Collections";
 import Header from "../../Components/Header/Header";
 import "./Home.css"
 
@@ -10,10 +11,28 @@ class Home extends React.Component {
             <div className="hotel-category container d-flex justify-content-between mt-4">
                 <CardHotelCategory />
                 <CardHotelCategory />
-
                 <CardHotelCategory />
-
             </div>
+
+            <div className="collections container">
+                <h2 className="collection-heading">Collections</h2>
+                <div className="d-flex justify-content-between collection-info-content">
+                    <span>Explore curated lists of top restaurants, cafes, pubs, and bars in Bengaluru, based on trends</span>
+                    <span className="right-info-collections">All collections in Bengaluru <i class="fa-solid fa-caret-right"></i></span>
+
+                </div>
+                <Collections />
+            </div>
+
+            <div className="popular-localities container mt-5">
+                <p className="localities-heading">Popular localities in and around Bengaluru</p>
+
+                <div class=" col-md-4 col-sm-6 col-xs-12 w-60 my-3 mx-2 border p-3">
+                    <h4 class="lead cut-text">Marathalli</h4>
+                    <span class="lead">444 places</span>
+                </div>
+            </div>
+
         </>
         )
     }
