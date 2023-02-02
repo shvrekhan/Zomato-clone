57b4636aec8c9bd243d0d10cab020a1926765bb9
shvrekhan <shvrekhan@gmail.com>
2023-02-02 11:23:28 +0530

    route added for hotel description page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import { connect } from "react-redux";
+import HotelCategoryWise from "./Pages/HotelsCategoryWise/HotelCategoryWise";
+import RestaurantPage from "./Pages/RestaurantPage/RestaurantPage";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact to="/">
+          <Route exact path="/">
             <Home />
           </Route>
+
+          <Route exact path="/online_order">
+            <HotelCategoryWise />
+          </Route>
+
+          <Route exact path="/restaurant/:id" render={(props) => {
+            return <RestaurantPage
+              {...props}
+              restaurant={this.props.restaurant} />
+          }} />
+
         </Switch>
       </BrowserRouter>
     )
   }
 }
 
-export default App;
+const mapStoreToProps = (stateInStore) => {
+  // console.log(stateInStore.hotels.hotels);
+  return {
+    restaurant: stateInStore.hotels.hotels,
+
+  }
+}
+
+export default connect(mapStoreToProps, {})(App);
