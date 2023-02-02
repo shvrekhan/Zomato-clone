d64b8ff24cae1d192ae16c5a6ccf6615655b430d
shvrekhan <shvrekhan@gmail.com>
2023-02-02 11:14:46 +0530

    hotel dexcription component added all items hotel have and the details about hotel added

---
+++ src/Components/HotelDescription/HotelDescription.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import "./HotelDescription.css"
+
+export default class HotelDescription extends Component {
+    render() {
+        return (
+            <>
+                {console.log(this.props.current, "okkkkkkkkkkk")}
+                <div className='container d-flex flex-column'>
+                    <div className='d-flex images-container gap-1 image-container-width'>
+                        <img className='w-75 p-1 cover' src={this.props.current.imgUrl} />
+                        <div className='d-flex flex-column w-25 mh-100 cover'>
+                            {this.props.current.items.map((currentItem) => {
+                                return <img src={currentItem.imgUrl} className="w-100 h-50 p-1" />
+                            })}
+                        </div>
+                    </div>
+                    <h2 className='hotel-name'>
+                        {this.props.current.title}
+                    </h2>
+
+                    <h3 className='hotel-cuisine-type'>
+                        {this.props.current.category[0]}
+                    </h3>
+
+                    <h4>
+                        {this.props.current.location}
+                    </h4>
+
+                    <div className='hotel-list'>
+                        <h3>Our Dishes</h3>
+
+                        <div className='d-flex'>
+                            {this.props.current.items.map((currentDish) => {
+                                return (
+                                    <div class="card" style={{ maxWidth: "18rem" }}>
+                                        <img class="card-img-top" src={currentDish.imgUrl} alt="Card image cap" />
+                                        <div class="card-body">
+                                            <h5 class="card-title">{currentDish.title}</h5>
+                                            <p class="card-text">{currentDish.description}</p>
+                                            <a href="#" class="btn btn-primary">Add to cart</a>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                </div>
+
+            </>
+        )
+    }
+}
