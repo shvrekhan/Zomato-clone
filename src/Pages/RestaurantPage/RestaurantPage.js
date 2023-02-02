6f92fac50e50da1ef0c25edd99c279a6a3981759
shvrekhan <shvrekhan@gmail.com>
2023-02-02 11:21:09 +0530

    hotel description and the dishes added

---
+++ src/Pages/RestaurantPage/RestaurantPage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import HotelDescription from '../../Components/HotelDescription/HotelDescription';
+import NavbarHotelPage from "../../Components/Navbar/NavbarHotelPage"
+
+export default class RestaurantPage extends Component {
+
+
+
+  render() {
+    return (
+      <>
+        <NavbarHotelPage />
+        {this.props.restaurant.map((current) => {
+          if (current.id === this.props.match.params.id) {
+            console.log(current, "okkkkkkk")
+            return <HotelDescription
+              title={current.title}
+              img={current.imgUrl}
+              items={current.item}
+              current={current} />
+          }
+        })}
+
+
+      </>
+
+    )
+  }
+}
