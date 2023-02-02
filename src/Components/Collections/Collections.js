1d400bfc84e74934502c9e32aa30eb7335ae0617
shvrekhan <shvrekhan@gmail.com>
2023-02-02 15:18:08 +0530

    console removed

--- src/Components/Collections/Collections.js
+++ src/Components/Collections/Collections.js
@@ -7,10 +7,10 @@ class Collections extends Component {
             <>
                 <div className="card col-lg-3 col-sm-4 my-3 collection-card bg-white d-flex flex-column text-white ">
                     <img className="card-img "
-                        src="https://b.zmtcdn.com/data/collections/1d1e838de3c921a6bdacb382a3d01c62_1667458986.jpg?output-format=webp"
+                        src={this.props.img}
                         alt="Card image" />
                     <div className="card-img-overlay overlay-text" >
-                        <h5 className="card-title lead" >16 newly opening restaurants</h5>
+                        <h5 className="card-title lead" >{this.props.subHead}</h5>
                     </div>
                 </div>
 
