3aaa18cf0f80c9c73c37dafd479e62fe447001e5
shvrekhan <shvrekhan@gmail.com>
2023-02-02 11:22:44 +0530

    collection hotelcategory reducer andded to root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 
 import users from "./users";
+import hotels from "./Hotels";
+import hotelCategory from "./HotelCategory";
+import collections from "./Collections"
+import topHotels from "./TopHotels"
 
-export default combineReducers({users});
+export default combineReducers({ users, hotels, hotelCategory, collections, topHotels });
 
