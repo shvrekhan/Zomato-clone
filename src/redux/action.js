a9113583f503c065daeddacc16d1998f0085ffe0
shvrekhan <shvrekhan@gmail.com>
2023-02-03 14:39:03 +0530

    action and action type of cart added

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,4 +1,4 @@
-import { ADD_USER, SEARCH } from "./actionType";
+import { ADD_USER, SEARCH, ADD_TO_CART, REMOVE_FROM_CART, DELETE_ITEM_FROM_CART } from "./actionType";
 
 export const addUser = (userName) => ({
     type: ADD_USER,
@@ -11,5 +11,24 @@ export const search = (searchInput) => ({
     type: SEARCH,
     payload: {
         searchInput,
+    }
+})
+
+export const addToCart = (item) => ({
+    type: ADD_TO_CART,
+    payload: {
+        item,
+    }
+})
+
+export const removeFromCart = (item) => ({
+    type: REMOVE_FROM_CART,
+    payload: item
+})
+
+export const deleteItemFromCart = (item) => ({
+    type: DELETE_ITEM_FROM_CART,
+    payload: {
+        item,
     }
 })
--- src/redux/actionType.js
+++ src/redux/actionType.js
@@ -1,2 +1,7 @@
 export const ADD_USER = "ADD_USER";
 export const SEARCH = "SEARCH";
+export const REMOVE = "REMOVE";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const DELETE_CART = "DELETE_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const DELETE_ITEM_FROM_CART = "DELETE_ITEM_FROM_CART";
