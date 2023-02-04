cedcf20aa0c42bb1b5aae8302ad511b3b2124817
shvrekhan <shvrekhan@gmail.com>
2023-02-04 14:48:53 +0530

    youorder page added

---
+++ src/Pages/YourOrders/YourOrder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavbarHotelPage from '../../Components/Navbar/NavbarHotelPage'
+
+export default class YourOrder extends Component {
+    render() {
+        return (
+            <>
+            
+                <NavbarHotelPage />
+                
+            </>
+        )
+    }
+}
